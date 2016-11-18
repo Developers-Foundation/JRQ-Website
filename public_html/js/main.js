e5814673fa78cabc85f6a9b72baea44cb4f6462c
Michael Sakamoto <sakamotomichael@hotmail.com>
2016-11-17 23:35:18 -0500

    Mobile Menu Change, Scrollspy + Smooth/Eased Scrolling Addition, Parallax Scrolling Addition, Menu Hover.css Addition, Updated About Description to Specifications

---
+++ public_html/js/main.js
@@ -0,0 +1,25 @@
+// Scrollspy Detection
+  $(document).ready(function(){
+      $('body').scrollspy({target: ".navbar", offset: 75});
+  });
+
+// Smooth Scrolling
+  $(function() {
+      $('a.nav-link').bind('click', function(event) {
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top
+          }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+      });
+  });
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// Mobile Menu Retracts After Clicking Link
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
